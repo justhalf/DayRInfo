9d252227b5c1b322e34b1de5a834f18288074b01
Aldrian Obaja Muis <aldrianobaja.m@gmail.com>
2021-05-06 01:25:00 +0800

    Add ability to give snapshot from map links

--- main.py
+++ main.py
@@ -12,13 +12,134 @@ from argparse import ArgumentParser
 import discord
 import logging
 import os
+import re
+from PIL import Image, ImageDraw
+from pathlib import Path
+from io import BytesIO
+from asyncio import create_task as run
 
 logging.basicConfig(level=logging.INFO)
 
 CLIENT_ID = '839181905249304606'
 PUBLIC_KEY = '8e3a6e541e5954298dc0087903037ef6d7c5480d599f2ae8c25d796af4e6ac25'
 TOKEN = None
-PREFIX = f'<@!{CLIENT_ID}>'
+
+RES_PATH = 'res'
+
+class Scope:
+    DIRECT = 'Direct'
+    MAP = 'Map'
+    NONE = 'None'
+
+MAP_REGEX = 'https://dayr-map.info/(?:index\.html)?\?(?:start\=true&)?clat\=([-0-9.]+)&clng\=([-0-9.]+)(?:&mlat\=([-0-9.]+)&mlng\=([-0-9.]+))?&zoom\=([-0-9.]+)'
+
+def get_scope(msg):
+    if re.match(f'^<@!?{CLIENT_ID}>.*$', msg):
+        return Scope.DIRECT
+    elif re.search(MAP_REGEX, msg):
+        return Scope.MAP
+    else:
+        return Scope.NONE
+
+class MapData:
+    """Stores the image of the world map"""
+    map_image = None
+    map_path = 'world_map_biomes_cities.png'
+
+    """Stores the image of the marker"""
+    marker_image = None
+    marker_path = 'marker_event.png'
+
+    """Data structure to store data about 
+    """
+    def __init__(self, clat, clng, zoom, mlat=None, mlng=None):
+        self.clat = clat
+        self.clng = clng
+        self.zoom = zoom
+        self.mlat = mlat
+        self.mlng = mlng
+        self.has_marker = self.mlat is not None
+
+    def __repr__(self):
+        return f'clat: {self.clat}, clng: {self.clng}, mlat: {self.mlat}, mlng: {self.mlng}, zoom: {self.zoom}'
+
+    __str__ = __repr__
+
+    def generate_snapshot(self, include_world=True):
+        """Generate a snapshot for this location.
+
+        include_world: If True, will include the world map at the bottom as the bigger picture
+        """
+        if self.has_marker:
+            y, x = -self.mlat, self.mlng
+        else:
+            y, x = -self.clat, self.clng
+        zoom = self.zoom
+        world = MapData.get_world_image()
+        top, bottom = y - 256/(2**zoom), y + 256/(2**zoom)
+        left, right = x - 256/(2**zoom), x + 256/(2**zoom)
+        logging.info(f'Cropping world at {left} {top} {right} {bottom}')
+        snapshot = world.crop((left, top, right, bottom))
+        if top - bottom <= 256:
+            snapshot = snapshot.resize((256, 256), resample=Image.NEAREST)
+        else:
+            snapshot = snapshot.resize((256, 256), resample=Image.BILINEAR)
+
+        if self.has_marker:
+            marker = MapData.get_marker_image()
+            snapshot.paste(marker, (112, 96), marker.getchannel('A'))
+
+        if include_world:
+            result = Image.new('RGBA', (256, 400))
+            result.paste(snapshot)
+            result.paste(world.resize((256, 144)), (0, 256))
+            if self.has_marker:
+                result.paste(marker, (int(x//32)-16, int(y//32)-32+256), marker.getchannel('A'))
+            overlay = Image.new('RGBA', (256, 400))
+            draw = ImageDraw.Draw(overlay)
+            fill_color = (255, 255, 0, 64) # Transparent yellow
+            outline_color = (255, 255, 0, 96) # More solid yellow
+            draw.rectangle((max(0, int(left//32)), max(0, int(top//32))+256, min(256, int(right//32)), min(144, int(bottom//32))+256), fill_color, outline_color, 2)
+            result = Image.alpha_composite(result, overlay)
+        else:
+            result = snapshot
+        
+        output = BytesIO()
+        result.save(output, format='png')
+        output.seek(0)
+        return output
+
+    def get_id(self):
+        if self.mlat:
+            return f'm{-self.mlat}_{self.mlng}'
+        else:
+            return f'{-self.clat}_{self.clng}'
+
+    @staticmethod
+    def from_match(match):
+        clat = float(match.group(1))
+        clng = float(match.group(2))
+        if match.group(3):
+            mlat = float(match.group(3))
+            mlng = float(match.group(4))
+        else:
+            mlat = mlng = None
+        zoom = float(match.group(5))
+        return MapData(clat, clng, zoom, mlat, mlng)
+
+    @classmethod
+    def get_world_image(cls):
+        if cls.map_image is None:
+            with open(str(Path(RES_PATH, cls.map_path)), 'rb') as infile:
+                cls.map_image = Image.open(infile).convert('RGBA')
+        return cls.map_image
+
+    @classmethod
+    def get_marker_image(cls):
+        if cls.marker_image is None:
+            with open(str(Path(RES_PATH, cls.marker_path)), 'rb') as infile:
+                cls.marker_image = Image.open(infile).convert('RGBA').resize((32, 32))
+        return cls.marker_image
 
 client = discord.Client()
 
@@ -31,23 +152,40 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(PREFIX):
-        logging.info(f'{dir(message)}')
-        logging.info(f'Received message: {message.content} from {message.author} at {message.created_at}')
+    logging.info(f'Received message: {message.content} from {message.author} at {message.created_at}')
+    scope = get_scope(message.content)
+    logging.info(f'Scope: {scope}')
+    if scope == Scope.DIRECT:
         await message.add_reaction('\U0001f44c')
         await message.channel.send(**{
-            'content': 'Hello too!',
+            'content': 'Hey there! I exist!',
             'reference': message.to_reference(),
             'mention_author': True,
-            # 'file': discord.File('4.png'),
-            'embed': discord.Embed.from_dict({
-                'title': 'An image',
-                'description': 'This is a description',
-                'image': {
-                    'url': 'https://dayr-map.info/map_tiles/0/7/4.png',
-                    }
-                }),
             })
+    elif scope == Scope.MAP:
+        add_map_emoji = run(message.add_reaction('\U0001F5FA'))
+        add_wait_emoji = run(message.add_reaction('\U000023F3'))
+        matches = re.finditer(MAP_REGEX, message.content)
+        for idx, match in enumerate(matches):
+            map_data = MapData.from_match(match)
+            logging.info(f'Generating image for {map_data}')
+            image = map_data.generate_snapshot()
+            snapshot_id = map_data.get_id().replace('_', ', ')
+            if snapshot_id[0] == 'm':
+                location_str = f'marker at -{snapshot_id[1:]}'
+            else:
+                location_str = f'center at -{snapshot_id}'
+            if idx == 0:
+                content = f'I see that you are posting a link to the interactive map. Here is a snapshot of that location ({location_str}).'
+            else:
+                content = ''
+            await message.channel.send(**{
+                'content': content,
+                'file': discord.File(image, filename=f'snapshot_{map_data.get_id()}.png'),
+                })
+        await add_wait_emoji
+        run(message.remove_reaction('\U000023F3', client.user))
+        await add_map_emoji
 
 def main(args=None):
     parser = ArgumentParser(description='')
